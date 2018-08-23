c64ade6a9da664a4abb71114cc3043a5736e9146
BMIND <albeirog82@gmail.com>
2018-08-22 19:40:42 -0500

    Se agregan ajustes para editar usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { DetalleUsuarioComponent } from './usuarios/detalle-usuario.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { EditarUsuarioComponent } from './usuarios/usuario-editar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsuariosComponent,
     DetalleUsuarioComponent, 
-    WelcomeComponent
+    WelcomeComponent,
+    EditarUsuarioComponent
   ],
   imports: [
     BrowserModule, 
     HttpClientModule, 
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: 'usuarios', component: UsuariosComponent }, 
       { path: 'usuarios/:id', component: DetalleUsuarioComponent }, 
+      { path: 'editarUsuario/:id', component: EditarUsuarioComponent }, 
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
--- src/app/usuarios/usuarios.service.ts
+++ src/app/usuarios/usuarios.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from "@angular/core";
 import { IUsuarios } from "./usuarios";
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs'; 
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
- 
+
 @Injectable({
-    providedIn: 'root'
-})
+    providedIn: 'root',
+  })
 export class UsuariosService{
 
-    private usuarioUrl = 'https://devencuestas.000webhostapp.com/api/usuario/read.php';
+    //private usuarioUrl = 'https://devencuestas.000webhostapp.com/api/usuario/read.php';
     //private usuarioUrl = 'api/products/usuarios.json';
-    constructor(private http: HttpClient) {}
+    private usuarioUrl = 'http://localhost/encuestas/api/usuario/read.php';
+    
+    constructor(private http: HttpClient) { }
 
     getUsuarios(): Observable<IUsuarios[]> {
-        return this.http.get<IUsuarios[]>(this.usuarioUrl).pipe(
-          tap(data => console.log('All: ' + JSON.stringify(data))),
-          catchError(this.handleError)  
-        );
-    }
+        return this.http.get<IUsuarios[]>(this.usuarioUrl)
+            .pipe(
+                tap(data => console.log(JSON.stringify(data))), 
+                catchError(this.handleError)
+            )
+     }
 
     getUsuario(id: number): Observable<IUsuarios | undefined >{
         return this.getUsuarios().pipe(
@@ -26,7 +31,39 @@ export class UsuariosService{
         );
     }
 
-    private handleError(err: HttpErrorResponse){
+    salvarUsuario(usuario: IUsuarios): Observable<IUsuarios> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        
+        if(+usuario.id === 0){
+            return this.createUsuario(usuario);
+        }
+        return this.actualizarUsuario(usuario); 
+    }
+
+    private createUsuario(usuario: IUsuarios): Observable<IUsuarios>{
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' })
+        usuario.id = null; 
+        return this.http.post<IUsuarios>(this.usuarioUrl, usuario, { headers: headers })
+            .pipe(
+                tap(data => console.log('Crear Usuario: ' + JSON.stringify(data) )),
+                catchError(this.handleError)
+            );
+
+    }
+
+    private actualizarUsuario(usuario: IUsuarios): Observable<IUsuarios>{
+        let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        const url = 'http://localhost/encuestas/api/usuario/update.php';
+        console.log('Enviando ' + JSON.stringify(usuario));
+        return this.http.post<IUsuarios>(url, JSON.stringify(usuario), { headers: headers })
+                    .pipe(
+                        tap(() => console.log('Actualizar producto ' + usuario.id )),
+                        map(() => usuario), 
+                        catchError(this.handleError)
+                    );
+    }
+
+    private handleError(err){
         let errorMessage = '';
         if(err.error instanceof ErrorEvent){
             errorMessage = 'Ocurrio un error : ${err.error.message}';
