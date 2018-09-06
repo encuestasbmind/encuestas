4d8de2cb050c28c3dd17c3c22846ff08e68d1cfe
Johan Davila <johan.davila@bmind.com>
2018-09-06 18:07:23 -0500

    se ha agrega una nueva variable llamada encuesta final

--- src/app/encuesta-final/encuesta-final.component.ts
+++ src/app/encuesta-final/encuesta-final.component.ts
@@ -1,67 +1,66 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute, Router } from '@angular/router';
 import { EventosService } from "../eventos/eventos.service";
+import { EncuestaFinalService } from "../encuesta-final/encuesta-final.service";
 import { EstudianteService } from "../estudiante/estudiante.service";
-import { IEstudiante} from "../estudiante/estudiante";
+import { IEstudiante } from "../estudiante/estudiante";
 import { IEvento } from "../eventos/eventos";
+import { IEventoEstudiante } from "../encuesta-parcial/eventoestuadiante";
 
 @Component({
-    selector: 'encuestas-encuestafinal', 
+    selector: 'encuestas-encuestafinal',
     templateUrl: './encuesta-final.component.html'
-}) 
-export class EncuestaFinalComponent{
+})
+export class EncuestaFinalComponent {
 
     eventoId: string;
     identificacion: string;
     errorMessage = '';
     evento: IEvento;
     estudiante: IEstudiante;
+    eventoEstudiante: IEventoEstudiante;
 
-    constructor(private route: ActivatedRoute, 
-                private router: Router,
-                private eventoService: EventosService,
-                private estudianteService: EstudianteService){}
-    
+    constructor(private route: ActivatedRoute,
+        private router: Router,
+        private eventoService: EventosService,
+        private estudianteService: EstudianteService,
+        private encuestaFinalService: EncuestaFinalService) { }
 
-    validarEncuestafinal(): void{
+
+    validarEncuestafinal(): void {
         this.errorMessage = '';
-        console.log('Iniciar validación de la encuesta con parametros ' + this.eventoId + ' ' + this.identificacion + '' + this.estudiante);
-        this.getEventoEstudiante(this.eventoId);
-        
-    }
+        this.encuestaFinalService.getEventoEstudiante(this.eventoId, this.identificacion).subscribe(
+            eventoEstudiante => {
+                this.eventoEstudiante = eventoEstudiante;
+                if (this.eventoEstudiante) {
+                    console.log('Iniciar validación de la encuesta con parametros ' + this.eventoId + ' ' + this.identificacion);
+                    this.getEventoEstudiantes(this.eventoId, this.identificacion);
 
-    getEventoEstudiante(eventoId: string): void{
-        this.eventoService.getOneEvento(+eventoId).subscribe(
-                evento => { 
-                        this.evento = evento;
-                        console.log('Recibido: ' + this.evento.id)
-                        if(this.evento.id){
+
+                } else {
+                    this.errorMessage = 'El evento o estudiante ingresado no existe';
+                }
+
+            }
+        );
+        }
+        
+     getEventoEstudiantes(eventoId: string, identificacion: string): void {
+
+                this.encuestaFinalService.getEventoEstudiante(eventoId, identificacion).subscribe(
+                    eventoEstudiante => {
+                        this.eventoEstudiante = eventoEstudiante;
+                        console.log('Recibido: ' + this.eventoEstudiante)
+                        if (this.eventoEstudiante) {
                             console.log('Datos validos');
-                            this.router.navigate(['/detalleencuestaparcial/'+this.evento.id]);
-                        }else{
+                            this.router.navigate(['' + this.eventoEstudiante]);
+                        } else {
                             console.log('El evento ingresado no existe');
                             this.errorMessage = 'El evento ingresado no existe';
                         };
-                },
-                error => this.errorMessage = <any>error);
-    }
-
-    getEstudiante(eventoId: string): void{
-        this.estudianteService.getOneEstudiante(+eventoId).subscribe(
-                estudiante => { 
-                        estudiante = estudiante;
-                        console.log('Recibido: ' + this.estudiante.id)
-                        if(this.estudiante.id){
-                            console.log('Datos validos');
-                            this.router.navigate(['/detalleencuestafinal/'+this.estudiante.id]);
-                        }else{
-                            console.log('El estudiante ingresado no existe');
-                            this.errorMessage = 'El estudiante ingresado no existe';
-                        };
-                },
-                error => this.errorMessage = <any>error);
-    }
-
+                    },
+                    error => this.errorMessage = <any>error);
+            }
 
 
 }
