fb2716c9f99df969c33b7dd30df91987101cd651
Johan Davila <johan.davila@bmind.com>
2018-09-24 11:41:44 -0500

    Se hacen modificaciones en la opcion encuesta parcial

--- src/app/encuesta-final/encuestafinal.ts
+++ src/app/encuesta-final/encuestafinal.ts
@@ -2,8 +2,8 @@ export interface IEncuestaFinal {
     id: string;
     instructor: string;
     estudiante_id: string;
-    nombres: string;
-    apellidos: string;
-    email: string; 
-    curso:string;
+    nombres:  string;
+    apellidos:  string;
+    email:  string; 
+    curso: string;
 }
